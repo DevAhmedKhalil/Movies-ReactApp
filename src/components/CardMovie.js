7304d7d708c833bd14fb21d8220e9aaf779779bf
DevAhmedKhalil <dev.akhalil01@gmail.com>
2024-12-15 16:52:27 +0200

    Finish CardMovie and make it dynamic show data from movies API.

--- src/components/CardMovie.js
+++ src/components/CardMovie.js
@@ -1,17 +1,21 @@
 import React from "react";
 import { Col } from "react-bootstrap";
 
-const CardMovie = () => {
+const CardMovie = ({ movie }) => {
   return (
     <Col xs="6" sm="6" md="4" lg="3" className="my-1">
       <div className="card">
-        <img src="coffee.jpg" className="card__image" alt="img" />
+        <img
+          src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+          className="card__image"
+          alt="img"
+        />
         <div className="card__overlay">
           <div className="overlay__text text-center w-100 p-2">
-            <p>إسم الفيلم : Coffee Corner</p>
-            <p>تاريخ الإصدار : 1995-3-2</p>
-            <p>النوع : إنيميشن</p>
-            <p>التقييم : 8.5</p>
+            <p>إسم الفيلم: {movie.title}</p>
+            <p>تاريخ الإصدار: {movie.release_date}</p>
+            <p>عدد المقيمين: {movie.vote_count}</p>
+            <p>التقييم: {movie.vote_average}</p>
           </div>
         </div>
       </div>
